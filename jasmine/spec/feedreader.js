53b36580b0769e0109e4dc895af55022f35d4f78
Mantas Radzvilavičius <m.radzvilavicius@gmail.com>
2018-06-27 22:19:30 +0300

    final commit

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -3,10 +3,7 @@
  * This is the spec file that Jasmine will read and contains
  * all of the tests that will be run against your application.
  */
-/* We're placing all of our tests within the $() function,
- * since some of these tests may require DOM elements. We want
- * to ensure they don't run until the DOM is ready.
- */
+
 $(function() {
     /* This is our first test suite - a test suite just contains
      * a related set of tests. This suite is all about the RSS
